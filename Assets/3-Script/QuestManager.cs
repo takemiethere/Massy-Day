8b1833c5cabfbc9c236864281098300d5442e292
ictsu631310440 <110732428+takemiethere@users.noreply.github.com>
2023-03-02 02:38:28 +0700

    add Set Map
    +จัดแมพใหม่ให้คัตซีน

--- Assets/3-Script/QuestManager.cs
+++ Assets/3-Script/QuestManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -39,5 +39,73 @@ public class QuestManager : MonoBehaviour
     {
         numItemsStolen++;
         UpdateItemsStolenText();
+    }
+}
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public int numItemsToSteal;
+    public int numItemsStolen;
+    public TextMeshProUGUI itemsStolenText;
+
+    public float theftTimeLimit = 5f;
+    public GameObject[] itemsToSteal;
+
+    private List<int> itemsRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numItemsStolen = 0;
+        itemsStolenText = GameObject.Find("ItemsStolenText").GetComponent<TextMeshProUGUI>();
+        UpdateItemsStolenText();
+
+        itemsRemaining = new List<int>();
+        for (int i = 0; i < itemsToSteal.Length; i++)
+        {
+            itemsRemaining.Add(i);
+        }
+    }
+
+    void UpdateItemsStolenText()
+    {
+        itemsStolenText.text = "Items Stolen: " + numItemsStolen + " / " + numItemsToSteal;
+    }
+
+    int GetRandomItem()
+    {
+        int randomIndex = Random.Range(0, itemsRemaining.Count);
+        int itemIndex = itemsRemaining[randomIndex];
+        itemsRemaining.RemoveAt(randomIndex);
+        return itemIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numItemsStolen >= numItemsToSteal)
+        {
+            // TODO: player has won the game, add win condition here
+        }
+    }
+
+    public void ItemStolen()
+    {
+        numItemsStolen++;
+        UpdateItemsStolenText();
+
+        if (numItemsStolen < numItemsToSteal)
+        {
+            int itemIndex = GetRandomItem();
+            GameObject itemToSteal = itemsToSteal[itemIndex];
+            Instantiate(itemToSteal, new Vector3(Random.Range(-5f, 5f), 0.5f, Random.Range(-5f, 5f)), Quaternion.identity);
+        }
     }
 }
