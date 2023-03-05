8ab06ec7350136bf12e10ec5d1f41f97efcd309e
ictsu631310440 <110732428+takemiethere@users.noreply.github.com>
2023-03-06 06:27:33 +0700

    fix Enemy Rat
    +แก้โค้ดเอไอหนู

---
+++ Assets/3-Script/DestroyOnMouseClick.cs
@@ -0,0 +1,8 @@
+/*using UnityEngine;
+
+public class DestroyOnMouseClick : MonoBehaviour
+{
+    public int requiredClicks = 3; // number of clicks required to destroy object
+    private int clicks = 0;
+}
+*/
